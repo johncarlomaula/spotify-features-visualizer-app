4d0764db6e125699ac43db6e63e4931148891ae3
John Carlo Maula <johncarlomaula@gmail.com>
2022-07-01 23:43:43 -0400

    fix secret keys

--- data.py
+++ data.py
@@ -7,9 +7,9 @@ import spotipy
 
 # Define constants for the Spotify Developer Account
 # Note: For the purpose of this project, I created a separate Spotify Developer Account.
-CLIENT_ID = st.secrets["spotify_client_id"]
-CLIENT_SECRET = st.secrets["spotify_client_id"]
-USER_NAME = st.secrets["spotify_username"]
+CLIENT_ID = st.secrets["SPOTIFY_ID"]
+CLIENT_SECRET = st.secrets["SPOTIFY_SECRET"]
+USER_NAME = st.secrets["SPOTIFY_USER"]
 
 # Define function to convert dataframe to a csv file
 @st.cache
